7ab5b5f25b66304db07d366aed6bad7b4cccd76d
Ettiene Mare <ettienemare@gmail.com>
2021-08-14 13:32:03 +0200

    Refactor

---
+++ Xml/Playground.Xml/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+using static System.Text.Json.JsonSerializer;
+using static Newtonsoft.Json.JsonConvert;
+
+namespace Playground.Xml {
+    public static class ExtensionMethods {
+        public static object? ToJson(this string xml) => 
+            string.IsNullOrWhiteSpace(xml) ? null : Deserialize<object>(SerializeXNode(XDocument.Parse(xml)).Replace("@", ""));
+
+        public static XElement? ToXml(this string xml) => 
+            string.IsNullOrWhiteSpace(xml) ? null : XDocument.Parse(xml).Root;
+    }
+}
