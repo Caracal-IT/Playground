acbe84c25ba4390e09d839b8d16b71f5e50ce096
Ettiene Mare <ettienemare@gmail.com>
2021-08-23 19:45:22 +0200

    refactor cancellation token

--- Playground.PaymentEngine/Extensions/StringExtensions.cs
+++ Playground.PaymentEngine/Extensions/StringExtensions.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Playground.PaymentEngine.Extensions {
     public static class StringExtensions {
-        public static async Task<string> ReadFromFileAsync(this string path, CancellationToken token) {
+        public static async Task<string> ReadFromFileAsync(this string path, CancellationToken cancellationToken) {
             using var sr = new StreamReader(path);
             return await sr.ReadToEndAsync();
         }
