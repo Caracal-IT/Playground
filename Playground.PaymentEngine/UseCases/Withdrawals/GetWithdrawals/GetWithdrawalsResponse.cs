905a781c03a49db03957eb8c46f4a1d3e24f1b8e
Ettiene Mare <ettienemare@gmail.com>
2021-08-26 07:46:56 +0200

    refactor code

--- Playground.PaymentEngine/UseCases/Withdrawals/GetWithdrawals/GetWithdrawalsResponse.cs
+++ Playground.PaymentEngine/UseCases/Withdrawals/GetWithdrawals/GetWithdrawalsResponse.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
 using static System.Array;
 
 namespace Playground.PaymentEngine.UseCases.Withdrawals.GetWithdrawals {
-    public record Withdrawal(long Id, long CustomerId, decimal Amount, long WithdrawalStatusId, DateTime DateRequested);
-    
     public record GetWithdrawalsResponse {
         public IEnumerable<Withdrawal> Withdrawals { get; set; } = Empty<Withdrawal>();
     }
