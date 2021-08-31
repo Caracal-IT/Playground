9803d62e9ec45e15b49b0a01a165e1fa63fbacde
Ettiene Mare <ettienemare@gmail.com>
2021-08-31 07:17:22 +0200

    Remove hard coding

--- Stores/Playground.PaymentEngine.Store.File/FileStore.cs
+++ Stores/Playground.PaymentEngine.Store.File/FileStore.cs
@@ -5,9 +5,9 @@ namespace Playground.PaymentEngine.Store.File {
     public abstract class FileStore {
         protected T GetRepository<T>() {
             var ns = GetType().Namespace!.Split('.');
-            //var path = Path.Join(ns[^3], ns[^2], ns[^1], "repository.xml");
-            var path = Path.Join("bin", "Debug", "net6.0", ns[^1], "repository.xml");
-            
+            var location = System.Reflection.Assembly.GetEntryAssembly()!.Location.Split('/');
+            var path = Path.Join(location[^4], location[^3], location[^2], ns[^1], "repository.xml");
+        
             using var fileStream = new FileStream(path, FileMode.Open);
             return (T) new XmlSerializer(typeof(T)).Deserialize(fileStream);
         }
