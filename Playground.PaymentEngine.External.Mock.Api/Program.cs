175e72e582368cc7affd9e5c8643f31ba02b8707
Ettiene Mare <ettienemare@gmail.com>
2021-09-02 13:05:42 +0200

    Remove redundant code

--- Playground.PaymentEngine.External.Mock.Api/Program.cs
+++ Playground.PaymentEngine.External.Mock.Api/Program.cs
@@ -19,8 +19,6 @@ if (app.Environment.IsDevelopment()) {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", () => "Hello World!");
-
 if (app.Environment.IsDevelopment()) {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
