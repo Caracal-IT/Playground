cc5e35453fd57adf6e63855f823c80830f0518c9
Ettiene Mare <ettienemare@gmail.com>
2021-09-02 08:36:28 +0200

    Fix path separator

--- Stores/Playground.PaymentEngine.Store.File/FileStore.cs
+++ Stores/Playground.PaymentEngine.Store.File/FileStore.cs
@@ -5,7 +5,7 @@ namespace Playground.PaymentEngine.Store.File {
     public abstract class FileStore {
         protected T GetRepository<T>() {
             var ns = GetType().Namespace!.Split('.');
-            var location = System.Reflection.Assembly.GetEntryAssembly()!.Location.Split(Path.PathSeparator);
+            var location = System.Reflection.Assembly.GetEntryAssembly()!.Location.Split(Path.DirectorySeparatorChar);
             var path = Path.Join(location[^4], location[^3], location[^2], ns[^1], "repository.xml");
         
             using var fileStream = new FileStream(path, FileMode.Open);
