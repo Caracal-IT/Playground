196f6f45327dd141e7f458219b2e4c8f069a3d39
Ettiene Mare <ettienemare@gmail.com>
2021-09-04 07:49:44 +0200

    Add tenant id

--- Playground.Core/Model/Entity.cs
+++ Playground.Core/Model/Entity.cs
@@ -3,5 +3,6 @@ using System.Xml.Serialization;
 namespace Playground.Core.Model {
     public record Entity {
         public long Id { get; set; }
+        public long TenantId { get; set; } = 1;
     }
 }
