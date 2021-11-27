d30255fb268e06f98b9e342b901df27626498b1e
ettienemare <ettienemare@gmail.com>
2021-11-27 08:41:46 +0200

    refactor code.

--- Applications/Playground.PaymentEngine.Application/UseCases/Payments/Process/ProcessUseCase.cs
+++ Applications/Playground.PaymentEngine.Application/UseCases/Payments/Process/ProcessUseCase.cs
@@ -58,24 +58,31 @@ public class ProcessUseCase {
             var req = new RoutingRequest(transactionId, nameof(ProcessUseCase), data.ToXml(), terminals!);
             var response = await _routingService.SendAsync(req, cancellationToken);
 
-            var result = response.Select(r => r.Result).Select(DeSerialize<ExportResponse>);
+            var result = response.Select(r => r.Result)
+                                 .Select(DeSerialize<ExportResponse>);
+            
             data.Response.AddRange(result);
         }
 
         async Task SaveResultsAsync() {
-            var results = items.SelectMany(i => i.Response).Select(_mapper.Map<TerminalResult>);
+            var results = items.SelectMany(i => i.Response)
+                               .Select(_mapper.Map<TerminalResult>);
+            
             await _terminalStore.LogTerminalResultsAsync(results, cancellationToken);
         }
 
         async Task UpdateStatusesAsync() {
-            await items.Select(SetStatusAsync).WhenAll(MaxConcurrentRequests);
+            await items.Select(SetStatusAsync)
+                       .WhenAll(MaxConcurrentRequests);
 
             async Task SetStatusAsync(ExportData data) {
                 var response = data.Response.FirstOrDefault(i => i.Code == "00") ?? new ExportResponse();
                 var statusId = response.Code == "00" ? 4 : 5;
                 var terminal = response.Terminal;
 
-                await data.Allocations.Select(SetAllocationStatusAsync).WhenAll(MaxConcurrentRequests);
+                await data.Allocations
+                          .Select(SetAllocationStatusAsync)
+                          .WhenAll(MaxConcurrentRequests);
 
                 async Task SetAllocationStatusAsync(ExportAllocation ea) =>
                     await _allocationStore.SetAllocationStatusAsync(ea.AllocationId, statusId, terminal!, data.Reference, cancellationToken);
@@ -94,18 +101,21 @@ public class ProcessUseCase {
                 });
 
         IEnumerable<ExportData> GetExportData() =>
-            allocations.Select(a => new ExportData {
-                Allocations = new List<ExportAllocation> { a },
-                Amount = a.Amount,
-                AccountTypeId = a.AccountTypeId,
-                Reference = hash5($"{a.AllocationId}"),
-                MetaData = a.MetaData
-            });
+            allocations
+                .Select(a => new ExportData {
+                    Allocations = new List<ExportAllocation> { a },
+                    Amount = a.Amount,
+                    AccountTypeId = a.AccountTypeId,
+                    Reference = hash5($"{a.AllocationId}"),
+                    MetaData = a.MetaData
+                });
     }
     
     private async Task<IEnumerable<ExportAllocation>> GetExportAllocationsAsync(IEnumerable<long> allocationIds, CancellationToken cancellationToken) {
         var accounts = _accountStore.GetAccounts();
-        var exportAllocations = await allocationIds.Select(GetExportAllocationAsync).WhenAll(MaxConcurrentRequests);
+        var exportAllocations = await allocationIds.Select(GetExportAllocationAsync)
+                                                   .WhenAll(MaxConcurrentRequests);
+        
         return exportAllocations.Where(a => a.AccountId > 0);
 
         async Task<ExportAllocation> GetExportAllocationAsync(long allocationId) {
