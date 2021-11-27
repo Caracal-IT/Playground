2bd6aead45774ba1c89491efcdf44f4549c7bd2f
ettienemare <ettienemare@gmail.com>
2021-11-27 08:55:40 +0200

    refactor code formatting

--- Applications/Playground.PaymentEngine.Application/UseCases/Payments/Process/ProcessUseCase.cs
+++ Applications/Playground.PaymentEngine.Application/UseCases/Payments/Process/ProcessUseCase.cs
@@ -10,7 +10,7 @@ using static Core.Hashing;
 using static Xml.Serialization.Serializer;
 
 public class ProcessUseCase {
-    private const short MaxConcurrentRequests = 50;
+    private const short MaxConcurrentRequests = 1;
     
     private readonly TerminalStore _terminalStore;
     private readonly IRoutingService _routingService;
@@ -29,10 +29,11 @@ public class ProcessUseCase {
 
     public async Task<ProcessResponse> ExecuteAsync(ProcessRequest request, CancellationToken cancellationToken) {
         var transactionId = Guid.NewGuid();
-        var allocations = await GetExportAllocationsAsync(request.Allocations, cancellationToken);
+        var allocations = await GetExportAllocationsAsync();
         var items = (request.Consolidate ? GetConsolidated() : GetExportData()).ToList();
 
-        await items.Select(ExportAsync).WhenAll(MaxConcurrentRequests);
+        await items.Select(ExportAsync)
+                   .WhenAll(MaxConcurrentRequests);
 
         await UpdateStatusesAsync();
         await SaveResultsAsync();
@@ -60,14 +61,14 @@ public class ProcessUseCase {
 
             var result = response.Select(r => r.Result)
                                  .Select(DeSerialize<ExportResponse>);
-            
+
             data.Response.AddRange(result);
         }
 
         async Task SaveResultsAsync() {
             var results = items.SelectMany(i => i.Response)
                                .Select(_mapper.Map<TerminalResult>);
-            
+
             await _terminalStore.LogTerminalResultsAsync(results, cancellationToken);
         }
 
@@ -91,7 +92,7 @@ public class ProcessUseCase {
 
         IEnumerable<ExportData> GetConsolidated() =>
             allocations
-                .GroupBy(a => new { customerId = a.CustomerId, accountId = a.AccountId })
+                .GroupBy(a => new {customerId = a.CustomerId, accountId = a.AccountId})
                 .Select(a => new ExportData {
                     Allocations = a.Select(i => i).ToList(),
                     Amount = a.Sum(i => i.Amount),
@@ -103,36 +104,36 @@ public class ProcessUseCase {
         IEnumerable<ExportData> GetExportData() =>
             allocations
                 .Select(a => new ExportData {
-                    Allocations = new List<ExportAllocation> { a },
+                    Allocations = new List<ExportAllocation> {a},
                     Amount = a.Amount,
                     AccountTypeId = a.AccountTypeId,
                     Reference = hash5($"{a.AllocationId}"),
                     MetaData = a.MetaData
                 });
-    }
-    
-    private async Task<IEnumerable<ExportAllocation>> GetExportAllocationsAsync(IEnumerable<long> allocationIds, CancellationToken cancellationToken) {
-        var accounts = _accountStore.GetAccounts();
-        var exportAllocations = await allocationIds.Select(GetExportAllocationAsync)
-                                                   .WhenAll(MaxConcurrentRequests);
-        
-        return exportAllocations.Where(a => a.AccountId > 0);
 
-        async Task<ExportAllocation> GetExportAllocationAsync(long allocationId) {
-            var allocations = await _allocationStore.GetAllocationsAsync(new[] { allocationId }, cancellationToken);
-            var allocation = allocations.FirstOrDefault() ?? new Allocation();
+        async Task<IEnumerable<ExportAllocation>> GetExportAllocationsAsync() {
+            var accounts = _accountStore.GetAccounts();
+            var exportAllocations = await request.Allocations
+                                                 .Select(GetExportAllocationAsync)
+                                                 .WhenAll(MaxConcurrentRequests);
 
-            var account = accounts.First(a => a.Id == allocation.AccountId);
-            
-            return new ExportAllocation
-            {
-                AllocationId = allocation.Id,
-                Amount = allocation.Amount + allocation.Charge,
-                AccountId = account.Id,
-                AccountTypeId = account.AccountTypeId,
-                CustomerId = account.CustomerId,
-                MetaData = _mapper.Map<List<MetaData>>(account.MetaData)
-            };
+            return exportAllocations.Where(a => a.AccountId > 0);
+
+            async Task<ExportAllocation> GetExportAllocationAsync(long allocationId) {
+                var allocationsInStore = await _allocationStore.GetAllocationsAsync(new[] {allocationId}, cancellationToken);
+                var allocation = allocationsInStore.FirstOrDefault() ?? new Allocation();
+
+                var account = accounts.First(a => a.Id == allocation.AccountId);
+
+                return new ExportAllocation {
+                    AllocationId = allocation.Id,
+                    Amount = allocation.Amount + allocation.Charge,
+                    AccountId = account.Id,
+                    AccountTypeId = account.AccountTypeId,
+                    CustomerId = account.CustomerId,
+                    MetaData = _mapper.Map<List<MetaData>>(account.MetaData)
+                };
+            }
         }
     }
 }
