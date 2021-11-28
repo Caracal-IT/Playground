b819ca32a69e27ae935090a9b9a032abdf924753
ettienemare <ettienemare@gmail.com>
2021-11-28 14:26:32 +0200

    Format script

--- Scripts/database-update.ps1
+++ Scripts/database-update.ps1
@@ -1,5 +1,11 @@
 Set-Location  '/Users/ettienemare/Documents/Development/CS/Playground/Stores/Playground.PaymentEngine.Store.EF'
 
+Write-Host 'Update Database'
+Write-Host 'Update EFAccountStore'
 dotnet ef database update --no-build --context EFAccountStore
+
+Write-Host 'Update EFCustomerStore'
 dotnet ef database update --no-build --context EFCustomerStore
+
+Write-Host 'Update EFDepositStore'
 dotnet ef database update --no-build --context EFDepositStore 
